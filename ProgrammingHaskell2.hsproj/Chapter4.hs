47c174c6c5cdd4e543424b65017e3c35d642dfd1
SokichiFujita <sokichi.fujita@gmail.com>
2019-09-01 15:58:04 +0900

    4.8.3

--- ProgrammingHaskell2.hsproj/Chapter4.hs
+++ ProgrammingHaskell2.hsproj/Chapter4.hs
@@ -18,13 +18,17 @@ abs' n = if n >= 0 then n else -n
 
 signum' :: Int -> Int
 signum' n = if n < 0 then -1 else
-  if n == 0 then 0 else 1
+            if n == 0 then 0 else 1
 
 -- 4.3 guard
 
 abs'' :: Int -> Int
 abs'' n | n >= 0 = n
         | n < 0  = -n
+
+abs''' :: Int -> Int
+abs''' n | n >= 0    =  n 
+         | otherwise = -n
 
 signum'' :: Int -> Int
 signum'' n | n < 0 = -1
@@ -67,21 +71,34 @@ halve xs = (take ((length xs) `div` 2) xs, drop ((length xs) `div` 2) xs)
 
 -- 4.8.2
 
+-- a
 third' :: [a] -> a 
 third' xs = head (tail (tail (take 3 xs)))
 
+-- b
 third'' :: [a] -> a 
 third'' xs = xs !! 2
 
+-- c
 third''' :: [a] -> a
 third''' (_: _: a: _) = a
 
 
--- 4/8/3
+-- 4.8.3
 
+-- a
+safetail' :: [a] -> [a]
+safetail' xs = if null xs then [] else tail xs
 
+-- b
+safetail'' :: [a] -> [a]
+safetail'' xs | null xs   = [] 
+              | otherwise = tail xs
 
-
+-- c
+safetail''' :: [a] -> [a]
+safetail''' [] = [] 
+safetail''' xs  = tail xs
 
 
 
