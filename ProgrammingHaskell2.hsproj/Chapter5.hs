45ff3a459b367e33ca3d6e62f643e09fd558cb2c
SokichiFujita <sokichi.fujita@gmail.com>
2019-09-02 17:41:31 +0900

    5.7.3

--- ProgrammingHaskell2.hsproj/Chapter5.hs
+++ ProgrammingHaskell2.hsproj/Chapter5.hs
@@ -51,6 +51,8 @@ count x xs = length [x' | x' <- xs, x == x']
 
 -- 5.5
 
+-- 5.5.1
+
 let2int :: Char -> Int
 let2int c = ord c - ord 'a'
 
@@ -64,16 +66,50 @@ shift n c | isLower c = int2let ((let2int c + n) `mod` 26)
 encode :: Int -> String -> String
 encode n xs = [shift n x | x <- xs]
 
+-- 5.5.2
+
+percent :: Int -> Int -> Float
+percent n m = (fromIntegral n / fromIntegral m) * 100
 
 
+freqs :: String -> [Float]
+freqs xs = [percent (count x xs) n | x <- ['a'..'z']]
+  where n = lowers xs
+
+-- 5.5.3
+
+chisqr :: [Float] -> [Float] -> Float
+chisqr os es = sum [((o-e)^2)/e | (o,e) <- zip os es]
+
+rotate :: Int -> [a] -> [a]
+rotate n xs = drop n xs ++ take n xs
+
+table :: [Float]
+table = [8.1, 1.5, 2.8, 4.2, 12.7, 2.2, 2.0, 6.1, 7.0,
+         0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0,
+         6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1]
+table' = freqs "kdvnhoo lv ixq"
+
+crack :: String -> String
+crack xs = encode (-factor) xs
+  where
+    factor = head (positions (minimum chitab) chitab)
+    chitab =  [chisqr (rotate n table') table | n <- [0..25]]
+    table' = freqs xs
 
 
+-- 5.7
 
 
+-- 5.7.2
 
+grid :: Int -> Int -> [(Int,Int)]
+grid x y = [(x,y)| x <- [0..x], y <- [0..y]]
 
+-- 5.7.3
 
-
+square :: Int -> [(Int,Int)]
+square x = [(a,b) | (a,b) <- grid x x, a = b]
 
 
 
