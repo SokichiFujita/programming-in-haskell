5f04a0ad90c47a98c3556d09f866782a2db4f7f9
SokichiFujita <sokichi.fujita@gmail.com>
2019-09-04 21:16:04 +0900

    6.8.6

--- ProgrammingHaskell2.hsproj/Chapter6.hs
+++ ProgrammingHaskell2.hsproj/Chapter6.hs
@@ -88,6 +88,51 @@ x ^^^ 1 = x
 x ^^^ n = x * x ^^^ (n -1)
 
 
+-- 6.8.4
+
+euclid :: Int -> Int -> Int
+euclid x y | x == y = x
+           | x >  y = euclid (x - y) y  
+           | x <  y = euclid (y - x) x
+
+
+-- 6.8.6
+
+-- 6.8.6.a
+
+and' :: [Bool] -> Bool
+and' l | []         <- l = False
+       | [True]     <- l = True
+       | (True:xs)  <- l = and' xs
+       | (False:xs) <- l = False
+
+-- 6.8.6.b
+
+concat' :: [[a]] -> [a]
+concat' []     = []
+concat' [[x]]  = [x]
+concat' (x:xs) = x ++ concat' xs
+
+-- 6.8.6.c
+
+replicate' :: Int -> a -> [a]
+replicate' 1 x = [x]
+replicate' n x = [x] ++ replicate' (n-1) x
+
+-- 6.8.6.d
+
+(!!!) :: [a] -> Int -> a
+(x: xs) !!! 0 = x
+(x: xs) !!! n = xs !!! (n-1)
+
+-- 6.8.6.e
+
+elem' :: Eq a => a -> [a] -> Bool
+elem' y []     = False
+elem' y (x:xs) | y == x = True
+               | y /= x = False
+
+
 
 
 
