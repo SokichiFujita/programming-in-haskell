a46f197641b93eb242fc103e1f1810ef104236f1
SokichiFujita <sokichi.fujita@gmail.com>
2019-09-07 18:27:08 +0900

    7.7.1

--- ProgrammingHaskell2.hsproj/Chapter7.hs
+++ ProgrammingHaskell2.hsproj/Chapter7.hs
@@ -1,5 +1,7 @@
 module Chapter7 where
-  
+
+import Data.Char
+import Data.List
 
 -- 7.1
 
@@ -33,4 +35,67 @@ twice' f = f . f
 
 id' :: a -> a
 id' = \x -> x
+
+-- 7.6
+
+type Bit = Int
+bin2int :: [Bit] -> Int
+bin2int bits = sum [w*b | (w,b) <- zip weights bits]
+  where weights = iterate (*2) 1
+
+bin2int' :: [Bit] -> Int
+bin2int' = foldr (\x y -> x + 2*y) 0
+
+make8 :: [Bit] -> [Bit]
+make8 bits = take 8 (bits ++ repeat 0)
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n `mod` 2 : int2bin (n `div` 2)
+
+encode :: String -> [Bit]
+encode = concat . map (make8 . int2bin . ord)
+
+chop8 :: [Bit] -> [[Bit]]
+chop8 []   = []
+chop8 bits = take 8 bits : chop8 (drop 8 bits)
+
+decode :: [Bit] -> String
+decode = map (chr . bin2int) . chop8
+
+transmit :: String -> String
+transmit = decode . channel . encode
+
+channel :: [Bit] -> [Bit]
+channel = id
+
+-- 7.7
+
+-- 7.7.1
+
+votes :: [String]
+votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
+
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (== x)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : rmdups (filter (/= x) xs)
+
+result :: Ord a => [a] -> [(Int,a)]
+result vs = sort [(count v vs, v) | v <- rmdups vs]
+
+winner :: Ord a => [a] -> a
+winner = snd . last . result
+
+
+
+
+
+-- 7.8
+
+
+
+
 
