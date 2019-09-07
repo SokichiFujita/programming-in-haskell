bba3e2f372a105847dde3bcde1ca7bd07c676370
SokichiFujita <sokichi.fujita@gmail.com>
2019-09-07 12:16:52 +0900

    7.5

--- ProgrammingHaskell2.hsproj/Chapter7.hs
+++ ProgrammingHaskell2.hsproj/Chapter7.hs
@@ -13,3 +13,24 @@ map' f l = [f x |x <- l]
 
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' f l = [x| x <- l, f x]
+
+-- 7.3
+
+sum' :: Num a => [a] -> a
+sum' = foldr (+) 0
+
+product' :: Num a => [a] -> a
+product' = foldr (*) 1
+
+-- 7.4
+
+sum'' :: Num a => [a] -> a
+sum'' = foldl (+) 0
+
+-- 7.5
+
+twice' f = f . f
+
+id' :: a -> a
+id' = \x -> x
+
