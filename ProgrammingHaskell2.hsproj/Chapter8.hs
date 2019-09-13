e85527de938d9c76b37f4044ff60336e656a5e78
SokichiFujita <sokichi.fujita@gmail.com>
2019-09-13 20:56:46 +0900

    8.2

--- ProgrammingHaskell2.hsproj/Chapter8.hs
+++ ProgrammingHaskell2.hsproj/Chapter8.hs
@@ -7,3 +7,20 @@ data Move = North | South | East | West deriving Show
 goWest :: Move -> Move
 goWest _ = West
 
+
+data Shape = Circle Float | Rect Float Float deriving Show
+
+square :: Float -> Shape
+square n = Rect n n
+
+area :: Shape -> Float
+area (Circle r) = pi * r^2
+area (Rect x y) = x * y
+
+safediv :: Int -> Int -> Maybe Int
+safediv _ 0 = Nothing
+safediv m n = Just (m `div` n)
+
+safehead :: [a] -> Maybe a
+safehead [] = Nothing
+safehead xs = Just (head xs)
